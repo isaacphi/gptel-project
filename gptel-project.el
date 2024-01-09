9a20f83badb225cd8dd526e87b5623b5ea48f926
Phil <phil@pheedloop.com>
2024-01-09 11:11:57 +0900

    slight refactor

--- gptel-project.el
+++ gptel-project.el
@@ -10,41 +10,51 @@
 ;; gptel-project is an Emacs extension that allows for collaboration
 ;; with Chat GPT inside Emacs. It leverages the gptel package to handle
 ;; communication with OpenAI's API.
-;
+                                        ;
 ;;; Code:
 (require 'gptel)
 (require 'projectile)
 
-;; Define a new function called 'my-gptel-setup-with-files'.
-(defun my-gptel-setup-with-files ()
-  "Setup a GPTel buffer with a list of project files."
-  ;; Mark this function for interactive use, enabling it to be called with M-x.
+(defun gptel-project-setup-with-files ()
   (interactive)
   ;; Check if Projectile is loaded (featurep 'projectile)
   ;; and if the current buffer is in a Projectile project (projectile-project-p).
   (unless (and (featurep 'projectile) (projectile-project-p))
-    ;; If not, raise an error to inform the user.
     (error "Projectile is not available, or you're not in a Projectile project"))
 
-  ;; Create and configure a new buffer specifically for GPTel.
-  (let ((gptel-buffer (get-buffer-create "*GPTel-Project-Files*")))
-    (with-current-buffer gptel-buffer
-      (org-mode)   ;; Enable Org mode, which is commonly used for structured notes.
-      (gptel-mode) ;; Enable the hypothetical 'gptel-mode'.
-
-    ;; Within the buffer, start inserting the content.
-    (let ((project-files (projectile-project-files (projectile-project-root))))
-      ;; Insert the project files, one on each line prefixed with "- ".
+  (let* ((buffer-name (read-string "Enter buffer name: " "*GPTel Project*"))
+         (buffer (get-buffer buffer-name)))
+    ;; If buffer exists, pop to the bottom and end the function.
+    (if buffer
+        (progn
+          (pop-to-buffer buffer)
+          (goto-char (point-max))
+          (return))
+      ;; Else, create the buffer.
+      (setq gptel-buffer (get-buffer-create buffer-name))
       (with-current-buffer gptel-buffer
-        (dolist (file project-files) ;; Loop over the list of project files.
-          (insert "- " file "\n")))) ;; Insert each file followed by a newline.
+        (org-mode)
+        (gptel-mode))))
 
-    ;; After the file list, prepare the buffer for chat input from the user.
+  ;; If a new buffer is created, continue with setup.
+  (when (buffer-live-p gptel-buffer)
+    ;; Within the buffer, start inserting the content.
     (with-current-buffer gptel-buffer
-      (insert "\n\n* Your chat input here\n") ;; Add a prompt for the user's input.
-      (goto-char (point-max))) ;; Move the cursor to the end of the buffer.
+      (insert (concat "* " (projectile-project-name) "\n"))
+      (insert (concat "** Files\n"))
+      ;; (projectile-project-unignored-files)
+      (let ((project-files (projectile-project-files (projectile-project-root))))
+        (dolist (file project-files)
+          (insert "- " file "\n")))
 
-    ;; Finally, display the newly populated buffer to the user.
-    (pop-to-buffer gptel-buffer)))
+      ;; After the file list, prepare the buffer for chat input from the user.
+      (insert (gptel-prompt-prefix-string))
+      (goto-char (point-max)))) ;; Move the cursor to the end of the buffer.
+
+  ;; Finally, display the newly populated buffer to the user.
+  (pop-to-buffer gptel-buffer))
+
+
+(provide 'gptel-project)
 
 ;;; gptel-project.el ends here
